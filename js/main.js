8ef9741902438681076b12b2502b6b5201a3921c
Adrian.Golebiewski <golebiewskiag@gmail.com>
2020-10-20 13:51:55 +0200

    jsBgShadow

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+///w tym momencie nasłuchujemy
+document.addEventListener('DOMContentLoaded', function() {
 
+    const nav = document.querySelector('.navbar')
 
-
+    function adShadow() {
+        if (window.scroll >= 300) {
+            nav.classList.add('shadow-bg')
+        } else {
+            nav.classList.remove('shadow-bg')
+        }
+    }
+    window.addEventListener('scroll')
+});
